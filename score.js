21f0ff5812c5f0a62832abb882afa91732ed6f37
mukundmadhav-kalvium <mukund.madhav@kalvium.community>
2023-10-25 20:39:46 +0530

    Done

--- score.js
+++ score.js
@@ -1,1 +1,13 @@
 // Iteration 8: Making scoreboard functional
+
+
+let scoreBoard = document.getElementById("score-board")
+let params = new URLSearchParams(window.location.search);
+let score = params.get("score")
+console.log("score: ",score);
+scoreBoard.textContent = score;
+
+let playAgainBtn = document.getElementById("play-again-button")
+playAgainBtn.onclick=()=>{
+    window.location.href = "./game.html";
+}
